0a4080b4e9f3d7372b56a7c4bc8de34f1a08110d
Patrick Whelan <patrickawhelan@gmail.com>
2020-02-05 15:40:47 -0500

    RESETING TO BASE

--- src/app.js
+++ src/app.js
@@ -48,48 +48,9 @@ app.use(express.static(path.join(__dirname, "../public")))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //your code below
-const creaturePath = path.join(__dirname, "../creatures.json")
-
-const getCreatures = () => {
-  let fileContents = fs.readFileSync(creaturePath).toString()
-  return JSON.parse(fileContents)
-}
 
 app.get("/", (req, res) => {
-  res.redirect("/creatures")
-})
-
-app.get("/creatures", (req,res) => {
-  res.render("index", {creatures:getCreatures()})
-})
-
-app.get("/creatures/new", (req,res) => {
-  creature = {name:"", age:"", ability:""}
-  res.render("new", {creature:creature})
-})
-
-app.get("/creatures/:creature", (req,res) => {
-  creatures = getCreatures()
-  creature = creatures.find(creature => {
-    return creature.name === req.params.creature
-  })
-  if (creature) {
-    res.render("show", {creature:creature})
-  } else {
-    res.status(404).send("You must have imagined it")
-  }
-})
-
-app.post("/creatures", (req,res) => {
-  let creaturesArray = getCreatures()
-  const newCreature = {name:req.body.name, age: req.body.age, ability: req.body.ability}
-  if (newCreature.name) {
-    creaturesArray.push(newCreature)
-    fs.writeFileSync(creaturePath, JSON.stringify(creaturesArray))
-    res.redirect("/")
-  } else {
-    res.render("new")
-  }
+  res.send("HI")
 })
 
 module.exports = app
